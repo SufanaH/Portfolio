85b6eef0c610c6689f352394326c3e5af1e7d7ca
SufanaH <sufana_1993@hotmail.com>
2023-12-09 05:45:13 +0300

    initial Commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import logo from "../../assets/images/logo.png";
+import navIcon1 from "../../assets/images/nav-icon1.svg";
+import navIcon2 from "../../assets/images/nav-icon2.svg";
+import navIcon3 from "../../assets/images/nav-icon3.svg";
+
+import "./_Footer.scss";
+
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col xs={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/sufana">
+                <img src={navIcon1} alt="linkidin-icon" />
+              </a>
+              <a href="https://github.com/SufanaH">
+                <img src={navIcon2} alt="gitHub-icon" />
+              </a>
+              <a href="https://twitter.com/Suufana">
+                <img src={navIcon3} alt="twitter-icon" />
+              </a>
+            </div>
+            <p>© Copyright 2023.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/hero/Banner.tsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import TrackVisibility from "react-on-screen";
+import headerImg from "../../assets/images/HeroSection.png";
+import "./_Banner.scss";
+
+const Banner: React.FC = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [, setIndex] = useState(1);
+  const toRotate = ["Full-Stack Developer", "Web Designer", "UI/UX Designer"];
+  const period = 2000;
+
+  useEffect(() => {
+    const ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text, delta]);
+
+  const tick = () => {
+    const i = loopNum % toRotate.length;
+    const fullText = toRotate[i];
+    const updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex((prevIndex) => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>
+                    {`Hi! I'm Sufana Alkushi`}{" "}
+                    <span
+                      className="txt-rotate"
+                      data-period="1000"
+                      data-rotate='[ "Full-Stack Developer", "Web Designer", "UI/UX Designer" ]'
+                    >
+                      <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
+                  <p>
+                    Aspiring Full-Stack developer with 2 years of experience as
+                    a Software Developer, supporting the digital transformation
+                    of Saudi companies. Possesses a blend of technical expertise
+                    in MERN Stack and UX/UI Design and effective communication
+                    skills for successful project outcomes. Committed to
+                    self-development and knowledge sharing, demonstrated through
+                    participation in various courses on product management and
+                    project management tailored for emerging companies.
+                    Additionally, engaged in mentoring activities and delivered
+                    several public speeches on blockchain-related topics
+                  </p>
+                  <button onClick={() => console.log("connect")}>
+                    Let’s Connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
+                  <img src={headerImg} alt="Header Img" />
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Banner;
